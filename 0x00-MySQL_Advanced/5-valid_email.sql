2fa2cceb9a5f5a000b74a3ea97303951895c932a
Alex Sifuna <aleksifuna@gmail.com>
2024-04-26 03:52:56 +0300

    added a script that creates a trigger that updates table users

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- creates a trigger that resets the attribute valid_email
+-- when only email has been changed
+DELIMITER $$
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+    BEGIN
+        IF OLD.email <> NEW.email THEN
+            SET NEW.valid_email = 0;
+        END IF;
+    END;
+$$
+DELIMITER ;
