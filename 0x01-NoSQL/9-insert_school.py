c988cdb0d9d6c94c2d179b7c62d1b6bb5f832f50
Alex Sifuna <aleksifuna@gmail.com>
2024-04-30 23:21:14 +0300

    added a script that inserts a document to a collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+"""
+Supplies one function insert_school
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection
+    """
+    results = mongo_collection.insert_one(kwargs)
+    return results.inserted_id
